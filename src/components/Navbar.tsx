a96b74b727f54eb94c97c801e8fc8cf22710b148
Saad Siddiq <siddiq.saad@gmail.com>
2023-07-05 03:31:09 -0500

    creating navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import './styles/Navbar.css';
+
+export const Navbar: React.FC = () => {
+  return (
+    <header id="navbar">
+      <nav>
+        <div>About</div>
+        <div>Projects</div>
+        <div>Contact</div>
+      </nav>
+    </header>
+  );
+}
