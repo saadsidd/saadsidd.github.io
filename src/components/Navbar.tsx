2b40cb2aab04d8c720d54d1dbb50cae630549c2c
Saad Siddiq <siddiq.saad@gmail.com>
2023-07-08 19:29:13 -0500

    added navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import './styles/Navbar.css';
+import { BsMoonStars, BsMoonStarsFill } from 'react-icons/bs';
 
 export const Navbar: React.FC = () => {
   return (
-    <header id="navbar">
-      <nav>
-        <div>About</div>
-        <div>Projects</div>
-        <div>Contact</div>
-      </nav>
+    <header id="nav">
+      <div className='nav-link'>About</div>
+      <div className='nav-link'>Projects</div>
+      <div className='nav-link'>Contact</div>
+      <div id='darkmode-icons-container'>
+        <BsMoonStars className='darkmode-icon' />
+        <BsMoonStarsFill className='darkmode-icon on' />
+      </div>
     </header>
   );
 }
