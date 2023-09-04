b50cef3240fa76d1f6c6cf0b3011746b4a198b55
Saad Siddiq <siddiq.saad@gmail.com>
2023-09-04 08:49:49 -0500

    added darkmode state

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import './App.css';
 import { Navbar } from './components/Navbar';
 import { ThreeCanvas } from './components/ThreeCanvas';
 import { MainContent } from './components/MainContent';
+import { useEffect, useState } from 'react';
+
 
 const App: React.FC = () => {
+
+  const [darkMode, setDarkMode] = useState('');
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
     <>
-      <Navbar />
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <main>
-        <ThreeCanvas />
+        <ThreeCanvas darkMode={darkMode} />
         <MainContent />
       </main>
     </>
